409fb94d443dc5f00d48606a8191a74efda09d41
WWAraujo <103542753+WWAraujo@users.noreply.github.com>
2023-03-22 10:09:11 -0300

    criação do tratamento de exeção na entrada valor da main

--- POO1/ProjetoBanco/src/Aplicacao.java
+++ POO1/ProjetoBanco/src/Aplicacao.java
@@ -261,62 +261,15 @@ public class Aplicacao {
         return 0;
     }
     public static double entradaValor(){
-        Scanner scanner = new Scanner(System.in);
-        imprimir("Digite o valor: ");
-        double valorDigitado = Double.parseDouble(scanner.nextLine());
-        return valorDigitado;
+        try {
+            Scanner scanner = new Scanner(System.in);
+            imprimir("Digite o valor: ");
+            double valorDigitado = Double.parseDouble(scanner.nextLine());
+            return valorDigitado;
+        } catch (Exception exception){
+            System.out.println("ERRO - Valor digitado nao é um numero!");
+        }
+        return 0;
     }
 
-
-//        boolean continuar = true;
-//        do {
-//            if (conta == 1) {
-//                System.out.println("1) Sacar Conta Corrente \n" +
-//                        "2) Depositar Conta Poupança \n" +
-//                        "3) Mostrar Saldo");
-//                int tipoCOnta = Integer.parseInt(scanner.nextLine());
-//                if (tipoCOnta == 1) {
-//                    cc.sacar(entradaValor());
-//                    cc.exibirSaldo();
-//                } else if (tipoCOnta == 2) {
-//                    cc.depositar(entradaValor());
-//                    cc.exibirSaldo();
-//                } else if (tipoCOnta == 3 ){
-//                    cc.exibirSaldo();
-//                } else {
-//                    System.out.println("Escolha 1, 2 ou 3");
-//                }
-//            } else if (conta == 2) {
-//                System.out.println("1) Sacar Conta Poupança\n" +
-//                        "2) Depositar Conta Poupança \n" +
-//                        "3) Mostrar Saldo");
-//                int tipoOperacao = Integer.parseInt(scanner.nextLine());
-//                if (tipoOperacao == 1) {
-//                    cp.sacar(entradaValor());
-//                    cp.exibirSaldo();
-//                } else if (tipoOperacao == 2) {
-//                    cp.depositar(entradaValor());
-//                    cp.exibirSaldo();
-//                } else if (tipoOperacao == 3) {
-//                    cp.exibirSaldo();
-//                }
-//            }
-//            System.out.println("Deseja continuar na mesma conta ? S ou N ");
-//            String ctnMesmaConta = scanner.nextLine().toUpperCase(Locale.ROOT);
-//            while (ctnMesmaConta.equals("N")) {
-//                System.out.println("Deseja troca de conta? S ou N");
-//                String ctnOutraConta = scanner.nextLine().toUpperCase(Locale.ROOT);
-//                if (ctnOutraConta.equals("N")) {
-//                    continuar = false;
-//                    break;
-//                }else if (ctnOutraConta.equals("S")){
-//                    System.out.println("Qual conta quer usar ? \n" +
-//                            "1) Conta Corrente \n" +
-//                            "2) Conta Poupança");
-//                    conta = Integer.parseInt(scanner.nextLine());
-//                    break;
-//                }
-//            }
-//        } while (continuar);
-//        System.out.println("Até logo "+ nome+" !");
 }
